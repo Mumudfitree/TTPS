bf54adc377c1cda03463e6b40d32703d33c1a579
Imron  Jehleh <ronza_jehleh2545@hotmail.com>
2021-07-22 00:57:55 +0700

    revert file ./login_db.php to commit 5cff457
    Because update of login_db.php, some files in notification folder doesn't go well. And I think code from commit 5cff457 are better that current file that have update in same way, but overcomplicated. So I revert back.
    
    Incompatible problem:
       Usage intechange between ['login_type'] in old file in line.notify branch, and ['Userlevel'] in file from Google Drive. Although both work in same way.
    
    Suggest:
       I have to recheck file from old and new files, and revalidate file.

--- login_db.php
+++ login_db.php
@@ -1,5 +1,5 @@
 <?php 
-    /*session_start();//คำสั่งต้องloginก่อนถึงเข้าได้
+    session_start();//คำสั่งต้องloginก่อนถึงเข้าได้
 
     require_once 'connection.php';
 
@@ -17,21 +17,22 @@
         $status;
   
         if (empty($username)) {
-            $errorMsg[] = "กรุณากรอกชื่อบัญชีผู้ใช้";
+            $errorMsg[] = "Please enter username";
         } else if (empty($password)) {
-            $errorMsg[] = "กรุณากรอกรหัสผ่าน";
+            $errorMsg[] = "Please enter password";
         } else if (empty($role)) {
-            $errorMsg[] = "กรุณาระบุบทบาท";
+            $errorMsg[] = "Please select role";
         } else if ($username AND $password AND $role) {
-           
-                
-                $strSQL = "SELECT username, password, user_role_id,status_master,fname,lname,master_id FROM login_information WHERE username = '".$username."' 
-	            AND password = '".$password." 'AND user_role_id = '".$role."' ";
-                $objQuery = mysqli_query($strSQL);
-                $objResult = mysqli_fetch_array($objQuery);
+            try {
+                $select_stmt = $db->prepare("SELECT username, password, user_role_id,status_master,fname,lname,master_id FROM login_information WHERE username = :uusername AND password = :upassword AND user_role_id = :urole");
+                $select_stmt->bindParam(":uusername", $username);
+                $select_stmt->bindParam(":upassword", $password);
+                $select_stmt->bindParam(":urole", $role);
+                //$select_stmt->bindParam(":ustatus'", $status);
 
+                $select_stmt->execute(); 
 
-                while($row =    $objResult) {
+                while($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
                     $dbusername = $row['username'];
                     $dbpassword = $row['password'];
                     $dbrole = $row['user_role_id'];
@@ -39,40 +40,46 @@
                     $dbfname = $row['fname'];
                     $dblname = $row['lname'];
                     $dbid = $row['master_id'];
-                }
+
             
                     
-                
+                }
                 if ($username != null AND $password != null AND $role != null ) {
-                        if ($username == $dbusername AND $password == $dbpassword AND $role == $dbrole AND $dbstatus == 'Active')  {
+                    if ($select_stmt->rowCount() > 0) {
+                        if ($username == $dbusername AND $password == $dbpassword AND $role == $dbrole AND $dbstatus == 'Active' AND $fname = $dbfname AND $lname = $dblname AND $id = $dbid)  {
                             switch($dbrole) {
                                 case '1':
-                                    $_SESSION['admin_login'] = $username;
-                                    $_SESSION['success'] = "ผู้ดูแลระบบ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
+                                    $_SESSION['login_type'] = 1;
+                                    $_SESSION['user_login'] = $username;
+                                    $_SESSION['success'] = "ยินดีต้อนรับผู้ดูแลระบบ";
                                     header("location: admin/admin_home.php");
                                 break;
                                 case '2':
-                                    $_SESSION['director_login'] = $username;
-                                    $_SESSION['success'] = "ผู้อำนวยการ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
+                                    $_SESSION['login_type'] = 2;
+                                    $_SESSION['user_login'] = $username;
+                                    $_SESSION['success'] = "Director... Successfully Login...";
                                     header("location: director/director_home.php");
                                 break;
                                 case '3':
-                                    $_SESSION['deputydirector_login'] = $username;
-                                    $_SESSION['success'] = "รองผู้อำนวยการ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
+                                    $_SESSION['login_type'] = 3;
+                                    $_SESSION['user_login'] = $username;
+                                    $_SESSION['success'] = "Deputy Director... Successfully Login...";
                                     header("location: deputydirector/deputydirector_home.php");
                                 break;
                                 case '4':
-                                    $_SESSION['academicdepartment_login'] = $username;
-                                    $_SESSION['success'] = "ฝ่ายวิชาการ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
+                                    $_SESSION['login_type'] = 4;
+                                    $_SESSION['user_login'] = $username;
+                                    $_SESSION['success'] = "Academic Department... Successfully Login...";
                                     header("location: academicdepartment/academicdepartment_home.php");
                                 break;
                                 case '5':
-                                    
-                                    $_SESSION['teacher_login'] = $username;
+
+                                    $_SESSION['login_type'] = 5;
+                                    $_SESSION['user_login'] = $username;
                                     $_SESSION['fname'] = $fname;
                                     $_SESSION['lname'] = $lname;
                                     $_SESSION['master_id'] = $id;
-                                    $_SESSION['success'] = "ครู ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
+                                    $_SESSION['success'] = "Teacher... Successfully Login...";
                                     header("location: teacher/teacher_home.php");
                                 break;
                                 default:
@@ -80,157 +87,18 @@
                                     header("location: index.php");
                             }
                         }else{
-                            
-                               // $_SESSION['admin_login'] = $username;
-                               // $_SESSION['success'] = "ผู้ดูแลระบบ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                               // header("location: admin/admin_home.php");
-                            
-                            //$_SESSION['error'] = "กรุณาตรวจสอบบัญชีผู้ใช้ รหัสผ่าน หรือบทบาทใหม่อีกครั้ง";
-                        //header("location: index.php");
+                            $_SESSION['error'] = "กรุณาตรวจสอบบัญชีผู้ใช้ รหัสผ่าน หรือบทบาทใหม่อีกครั้ง";
+                        header("location: index.php");
                         }
-                    
+                    } else {
+                        $_SESSION['error'] = "กรุณาตรวจสอบบัญชีผู้ใช้ รหัสผ่าน หรือบทบาทใหม่อีกครั้ง";
+                        header("location: index.php");
+                    }
                 }
-            
-        
+            } catch(PDOException $e) {
+                $e->getMessage();
+            }
+        }
     }
-    }*/
+
 ?>
-<?php 
-session_start();
-        if(isset($_POST['btn_login'])){
-				//connection
-                  include("connection.php");
-				//รับค่า user & password
-                  $username = $_POST['txt_username'];
-                  $password = $_POST['txt_password'];
-                  $role = $_POST['txt_role'];
-				//query 
-                  $sql="SELECT * FROM login_information Where username='".$username."' and password='".$password."' and user_role_id ='".$role."' ";
-
-                  $result = mysqli_query($conn,$sql);
-				
-                  if(mysqli_num_rows($result)==1){
-
-                      $row = mysqli_fetch_array($result);
-
-                      $_SESSION["UserID"] = $row["master_id"];
-                      $_SESSION["User"] = $row["fname"]." ".$row["lname"];
-                      $_SESSION["Userlevel"] = $row["user_role_id"];
-                      
-
-                      if($_SESSION["Userlevel"]== "1" && $row['status_master'] == 'Active'  ){ //ถ้าเป็น admin ให้กระโดดไปหน้า admin_page.php
-
-                        $_SESSION['admin_login'] = $username;
-                        $_SESSION['success'] = "ผู้ดูแลระบบ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                        header("location: admin/admin_home.php");
-
-                      }else{
-                        echo "<script>";
-                            echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                            echo "window.history.back()";
-                        echo "</script>";
-    
-                      }
-
-                      if ($_SESSION["Userlevel"]=="2" && $row['status_master'] == 'Active' ){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
-
-                        $_SESSION['director_login'] = $username;
-                        $_SESSION['success'] = "ผู้อำนวยการ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                        header("location: director/director_home.php");
-
-                      }else{
-                        echo "<script>";
-                            echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                            echo "window.history.back()";
-                        echo "</script>";
-    
-                      }
-
-                      if ($_SESSION["Userlevel"]=="3" && $row['status_master'] == 'Active'){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
-
-                        $_SESSION['deputydirector_login'] = $username;
-                        $_SESSION['success'] = "รองผู้อำนวยการ ฝ่ายวิชาการ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                        header("location: deputydirector/deputydirector_home.php");
-
-                      }else{
-                        echo "<script>";
-                            echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                            echo "window.history.back()";
-                        echo "</script>";
-    
-                      }
-
-                      if ($_SESSION["Userlevel"]=="4" && $row['status_master'] == 'Active'){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
-
-                        $_SESSION['academicdepartment_login'] = $username;
-                        $_SESSION['success'] = "ฝ่ายวิชาการ ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                        header("location: academicdepartment/academicdepartment_home.php");
-
-                      }else{
-                        echo "<script>";
-                            echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                            echo "window.history.back()";
-                        echo "</script>";
-    
-                      }
-                      
-
-                      if ($_SESSION["Userlevel"]=="5" && $row['status_master'] == 'Active'){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
-
-                        $_SESSION['teacher_login'] = $username;
-                        $_SESSION["User"];
-                        $_SESSION['master_id'] = $id;
-                        $_SESSION['success'] = "ครู ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                        header("location: teacher/teacher_home.php");
-
-                      }else{
-                        echo "<script>";
-                            echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                            echo "window.history.back()";
-                        echo "</script>";
-    
-                      }
-
-                      if ($_SESSION["Userlevel"]=="6" && $row['status_master'] == 'Active'){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
-
-                        $_SESSION['headprimary_login'] = $username;
-                        $_SESSION['success'] = "หัวหน้าช่วงชั้นประถม ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                        header("location: headprimary/headprimary_home.php");
-
-                      }else{
-                        echo "<script>";
-                            echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                            echo "window.history.back()";
-                        echo "</script>";
-    
-                      }
-                      if ($_SESSION["Userlevel"]=="7" && $row['status_master'] == 'Active'){  //ถ้าเป็น member ให้กระโดดไปหน้า user_page.php
-
-                        $_SESSION['headhighschool_login'] = $username;
-                        $_SESSION['success'] = "หัวหน้าช่วงชั้นมัธยม ... ดำเนินการเข้าสู่ระบบเสร็จสิ้น";
-                        header("location: headhighschool/headhighschool_home.php");
-
-                      }else{
-                        echo "<script>";
-                            echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                            echo "window.history.back()";
-                        echo "</script>";
-    
-                      }
-
-                  }else{
-                    echo "<script>";
-                        echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                        echo "window.history.back()";
-                    echo "</script>";
-                  }
-
-                }else{
-                  echo "<script>";
-                      echo "alert(\" ชื่อบัญชีผู้ใช้ รหัสผ่าน หรือ ระบุบทบาท ไม่ถูกต้อง\");"; 
-                      echo "window.history.back()";
-                  echo "</script>";
-                }
-?>
-
-    
