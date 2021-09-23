7ce9541bdc7c371d45ef57e395113156348746f4
Imron  Jehleh <ronza_jehleh2545@hotmail.com>
2021-09-23 09:41:42 +0700

    Remove duplicate codes
        line 23 is already called at line 8. (But use different method. Line 8 use as function, but line 23 use as object.)

--- connection.php
+++ connection.php
@@ -19,13 +19,8 @@ if (!$conn) {
     $db_user = "root"; // database username
     $db_password = ""; // database password
     $db_name = "php_multiplelogin"; // database name
+    
     // Create connection
-    $conn = new mysqli($db_host, $db_user, $db_password,$db_name);
-    mysqli_query($conn, "SET NAMES 'UTF8' ");
-    //Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
     try {
 
         $db = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_user, $db_password);
