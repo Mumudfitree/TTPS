c063ddf4951a37e26b6502cdbf439acc9b1cc54b
Imron  Jehleh <ronza_jehleh2545@hotmail.com>
2021-09-23 13:52:15 +0700

    remove duplicate codes.
        CHANGED:
          remove duplicate variable declaration.
          23 - 28: remove duplicate codes. (first is declared through function. Second one is declared as object.)
          8: now function will called old variables.

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "multiplelogin_v4";
+$db_host = "localhost"; // localhost server
+$db_user = "root"; // database username
+$db_password = ""; // database password
+$db_name = "php_multiplelogin"; // database name
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 mysqli_set_charset($conn, "utf8");
 
 // Check connection
@@ -15,17 +15,8 @@ if (!$conn) {
 
     //อันเก่าแบบPDO
 
-    $db_host = "localhost"; // localhost server
-    $db_user = "root"; // database username
-    $db_password = ""; // database password
-    $db_name = "multiplelogin_v4"; // database name
+
     // Create connection
-    $conn = new mysqli($db_host, $db_user, $db_password,$db_name);
-    mysqli_query($conn, "SET NAMES 'UTF8' ");
-    //Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
     try {
 
         $db = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_user, $db_password);
