2cefd83ca1dcc20a0c31f7c1c25877132418fa0b
Imron  Jehleh <ronza_jehleh2545@hotmail.com>
2021-10-15 23:06:38 +0700

    added function for doing generating characters, and generating text.
        ADDED:
          added charGenerate() and nameGenerate().
            charGenerate() generate char according to specified mode. (I didn't decide what to use as default)
              It also support to generate space too.
            nameGenerate() generate string according to stringLength. By calling charGenerate();

--- report_table/src/userJsonHandle.php
+++ report_table/src/userJsonHandle.php
@@ -23,4 +23,41 @@
         return $value;
     }
 
+    function charGenerate($mode){ //mode: 2 letter (start with 1), enable spacebar;
+                                  //      end letter: 1- only UPPERCASE 2- only lowercase 3- mixed
+
+        switch(gettype($mode[0])){
+            case 'i': //'i' from "integer"
+                $mode = strval($mode);
+                break;
+            case 's': //'s' from "string"
+                break;
+            default:
+                return 1;
+        }
+
+        $charPool = (isset($mode[1])) ? 27 : 26;
+        $generateMode = (isset($mode[1])) ? $mode[1] : $mode[0];
+
+        switch($generateMode){
+            case '0':
+                $value = randomScope(65, 90);
+                break;
+            case '1':
+                $value = randomScope(97, 122);
+                break;
+        }
+        
+
+    }
+
+    function nameGenerate(int $numberOfName, ?int $nameLength){
+
+        if(!isset($nameLength)){
+            $nameLength = 20;
+        }
+
+
+    }
+
 ?>
